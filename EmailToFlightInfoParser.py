3af5d6b792cc0d4030576357da52709b7aaa74cf
Shantanu Dey Anik <40882871+Shantanu2645@users.noreply.github.com>
2023-04-11 20:59:56 +0600

    Update EmailToFlightInfoParser.py

--- EmailToFlightInfoParser.py
+++ EmailToFlightInfoParser.py
@@ -12,6 +12,8 @@ from org.apache.nifi.processor.io import InputStreamCallback
 from org.apache.nifi.processor.io import StreamCallback
 from org.apache.nifi.processor.io import OutputStreamCallback
 
+
+
 class PyInputStreamCallback(InputStreamCallback):
     _text = None
  
@@ -25,7 +27,6 @@ class PyInputStreamCallback(InputStreamCallback):
         self._text = IOUtils.toString(inputStream, StandardCharsets.UTF_8)
  
 flowFile = session.get()
-
 
 class PyOutputStreamCallback(OutputStreamCallback):
     def __init__(self, data):
@@ -41,6 +42,7 @@ def process_data(email):
     uid = str(uuid.uuid4())
     data = re.sub(r'^\s+', '', email, flags=re.MULTILINE)
     data = re.sub(r'=C2=A5','',data,flags=re.MULTILINE)
+    data = re.sub(r'=20','',data,flags=re.MULTILINE)
     data = data.split('\n')
     data = [re.sub(r'\s+', ' ', line).strip() for line in data]
     header = data[:2]
@@ -60,6 +62,8 @@ def process_data(email):
     email_gist = '\n'.join(data[2:])
 
     cleaned_email_gist = email_gist.split('LFTD')
+    # return email_gist
+
     parsed_data = []
 
     for i in cleaned_email_gist:
@@ -69,16 +73,29 @@ def process_data(email):
             if re.match(pregex,items):
                 p_info = items
 
-                pax_details = {
-                    "id": 0,
-                    "pax_master_id": uid,
-                    "pax_name": p_info.split('/')[7]+"/"+p_info.split('/')[8],
-                    "passport_no": p_info.split('/')[2],
-                    "ticket_no": i.split("\n")[-1].split(" ")[-6], 
-                    "is_archived": 0,
-                    "docs": re.sub("\n"," ", i)
-                }
+                try:
 
+                    pax_details = {
+                        "id": 0,
+                        "pax_master_id": uid,
+                        "pax_name": p_info.split('/')[7]+"/"+p_info.split('/')[8],
+                        "passport_no": p_info.split('/')[2],
+                        "ticket_no": i.split("\n")[-1].split(" ")[-6], 
+                        "is_archived": 0,
+                        "docs": re.sub("\n"," ", i)
+                    }
+
+                except IndexError:
+
+                    pax_details = {
+                        "id": 0,
+                        "pax_master_id": uid,
+                        "pax_name": p_info.split('/')[7]+"/"+p_info.split('/')[8],
+                        "passport_no": p_info.split('/')[2],
+                        "ticket_no": "Ticket Number Not Found", 
+                        "is_archived": 0,
+                        "docs": re.sub("\n"," ", i)
+                    }
                 parsed_data.append(pax_details)
             else:
                 pass
@@ -87,7 +104,8 @@ def process_data(email):
     main_data ={
         "pax_master": pax_master,
         "pax_details": parsed_data
-    }
+    }  
+
     return json.dumps(main_data)
 
 
@@ -114,7 +132,5 @@ if flowFile is not None :
     # added this two lines 
     write_cb = PyOutputStreamCallback(output)
     flowFile = session.write(flowFile, write_cb)
-    
-       
+          
     session.transfer(flowFile, ExecuteScript.REL_SUCCESS)# your code goes here
-
